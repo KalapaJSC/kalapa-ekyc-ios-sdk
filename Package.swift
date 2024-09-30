7c8184b8b1fa86e925a6957ad56edbcf6f10f20d
Tran Hong Quan <quanth@kalapa.vn>
2024-09-30 12:13:35 +0700

    Update KalapaEkycSDK

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .binaryTarget(
             name: "KalapaEkycSDK",
             url: "https://raw.githubusercontent.com/KalapaJSC/kalapa-ekyc-ios-sdk/master/KalapaEkycSDK/2.7.0/KalapaSDK.xcframework.zip",
-            checksum: "f050deae569439c9a783f8009ca02a389f4cea41380304d179ddcab2faad0db4"
+            checksum: "2954919f4f19f8289556e1284a41413c05593a75a4a133a01b34815bb1bcb76c"
         )
     ]
 )
