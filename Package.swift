96adfe4b680fa7cf0942317b65f44cab12b1b600
Tran Hong Quan <quanth@kalapa.vn>
2025-02-25 09:13:05 +0700

    Update semi-active liveness flow and add passport support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KalapaEkycSDK",
-            url: "https://raw.githubusercontent.com/KalapaJSC/kalapa-ekyc-ios-sdk/master/KalapaEkycSDK/2.7.6/KalapaSDK.xcframework.zip",
-            checksum: "92a92249e97b231d81f1396b760c700c24b8dbf809c7ddcedfbc647fe787726f"
+            url: "https://raw.githubusercontent.com/KalapaJSC/kalapa-ekyc-ios-sdk/master/KalapaEkycSDK/2.7.7/KalapaSDK.xcframework.zip",
+            checksum: "0e41b05762ba7280601d7e286d5959961caab7c3c722d09076d8026ec030cca7"
         )
     ]
 )
